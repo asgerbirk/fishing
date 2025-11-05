1eb60cba16f4830a6a846be3b8844e7d3551027f
asgerbirk <asger.birk@gmail.com>
2025-11-05 21:09:23 +0100

    test

--- mqtt_sensors.js
+++ mqtt_sensors.js
@@ -1,22 +1,15 @@
-function setup() {
-
-}
-
+function setup() {}
 
 function draw() {
-
-  if(orientationSensor.hasNewValue) 
-  { 
+  if (orientationSensor.hasNewValue) {
     let gyro = orientationSensor.get();
     let alpha = gyro.alpha; // alpha, beta & gamma
-    console.log(alpha);
+    console.log("SENDT (alpha): " + alpha); // <-- Ændret
     sender(alpha);
-    
   }
 }
 
-const threshold = 10; 
-function touchStarted() 
-{ 
-setupOrientation(threshold); 
+const threshold = 10;
+function touchStarted() {
+  setupOrientation(threshold);
 }
--- receiver.js
+++ receiver.js
@@ -1,7 +1,6 @@
 function modtager(topic, modtagetBesked) {
-    let modtagerBuffer = JSON.parse(modtagetBesked);
-    let afsender = modtagerBuffer.from;
-    let value = modtagerBuffer.val;
-    console.log(value);
-    // gør noget med value
+  let modtagerBuffer = JSON.parse(modtagetBesked);
+  let afsender = modtagerBuffer.from;
+  let value = modtagerBuffer.val;
+  console.log("MODTAGET fra " + afsender + ": " + value); // <-- Ændret  // gør noget med value
 }
