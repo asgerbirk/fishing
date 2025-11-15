0bb8c42d3c7fb90b661926a3b5e21f8da6e63eb7
asgerbirk <asger.birk@gmail.com>
2025-11-15 23:33:16 +0100

    changed topic

--- q_functions.js
+++ q_functions.js
@@ -1,6 +1,6 @@
 // --------- MQTT FUNKTIONER -------moja2024 ------
 
-const topic = "ASGBITOPIC";
+const topic = "ASGBI";
 const afsenderID = "P5";
 
 const client = mqtt.connect("wss://broker.hivemq.com:8884/mqtt");
